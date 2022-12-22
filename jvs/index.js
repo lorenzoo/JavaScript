9b9ec004c4c02a91673859a9e00730f02e5f94ac
lorenzoo <enriquelorenzoo@gmail.com>
2022-12-22 23:02:40 +0100

    Cambios pre-entrega_2

--- jvs/index.js
+++ jvs/index.js
@@ -1,6 +1,133 @@
+const tokens = []//Defino el Array de productos
+
+/* ESTOS SON LOS VALORES QUE YO PONDRIA POR DEFECTO
+  {id:1, nombre:"Platino", precio: 1000},
+  {id:2, nombre:"Oro", precio: 800},
+  {id:3, nombre:"Plata", precio: 600},
+  {id:4, nombre:"Cobre", precio: 300},
+  {id:5, nombre:"Estaño", precio: 100},
+*/
+
+const tokens_comprados = []; //Defino los productos que compro
+
+class Producto {
+  constructor (id, nombre, precio) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+
+  }
+}
+// Busqueda de token
+function seleccionarToken(id) {
+  return (tokens.find(item => item.id === id) || null);
+}
+
+function meterEnCompras(token) {
+  tokens_comprados.push(token);
+}
+
+let cargarToken = true;
+
+while (cargarToken) { //Valores de los tokens libres escribir
+  let id_token = tokens.length + 1;
+  let nombre_token = prompt("Nombre del Token:");
+  let precio_token = parseFloat(prompt("Precio del Token"));
+  let token = new Producto(id_token, nombre_token, precio_token);
+
+  //Agregar tokens al Array de tokens
+  tokens.push(token);
+  //Cortar subida de datos
+  cargarToken = confirm("Quieres añadir mas Tokens?");
+  
+} 
+
+ cargarToken = true;
+ 
+ 
+ while (cargarToken) {
+  let contenido_tokens = "";
+  for (let token of tokens) {
+    contenido_tokens += token.id + "- " + token.nombre + "€" + token.precio + "\n";
+
+
+  }
+  //Indicamos token ID
+  let id_token = parseInt(prompt("Selecciona tu Token:\n" + contenido_tokens));
+  
+  //Buscamos el token
+  let token = seleccionarToken(id_token);
+  
+  if (token != null) {
+    //Añadimos token a la compra
+    meterEnCompras(token)
+  } else {
+    alert("Este Token con ID numero: " + id_token + " no esta disponible")
+
+    }
+  
+  
+  
+
+    cargarToken = confirm("Quieres añadir mas Tokens?");
+  }
+ 
+let suma_total = 0;
+
+for (let tok of tokens_comprados) {
+     let token = new Producto(tok.id, tok.nombre, tok.precio);
+     suma_total += token.precio;
+     alert("Total Token:\n\n" + contenido_tokens + "€" + suma_total);
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*Me gustaria crear un juego de azar, empezamos por recolectar datos basicos, nombre, apellido, sexo, etc..
 Aqui tenemos un algoritmo con condicional ( function ) */
-let sexo = prompt('Hola, es usted Hombre o Mujer?').toLocaleLowerCase()
+/* let sexo = prompt('Hola, es usted Hombre o Mujer?').toLocaleLowerCase()
 let nombre = prompt('Introduzca su nombre')
 let apellido = prompt('Introduzca su apelllido' + ' ' + nombre)
 function validar_sexo(sexo) {
@@ -41,7 +168,7 @@ if (sexo == 'hombre') {
       '?',
   )
 }
-const idioma = ['castellano', 'frances', 'ingles', 'portuges','aleman']; // Aqui tenemos nuestro array de idiomas, la idea es que cada jugador seleccione su idioma ( mas adelante para poder explicar las instrucciones del juego en su idioma) este array podria ir al inicio del todo para que ya empiece con sus datos basicos.
+ // Aqui tenemos nuestro array de idiomas, la idea es que cada jugador seleccione su idioma ( mas adelante para poder explicar las instrucciones del juego en su idioma) este array podria ir al inicio del todo para que ya empiece con sus datos basicos.
 console.log(idioma[2]); // Aqui tenemos en consola lo que podria ser una llamda de array al idioma Ingles.
 //Aqui he hecho el algoritmo ciclo del ejercicio para la entrega
 var jugador = {sexo, nombre, apellido}; // Esot es un bojeto con los datos del jugador 
@@ -100,4 +227,5 @@ function juego(adivina_numero) {
     }
   }
 }
-juego(adivina_numero)
+juego(adivina_numero) */
+
