b0fdab09ae3b68e5597c49454f1420d386d1afa1
enrique lorenzo <enriquelorenzoo@gmail.com>
2022-12-23 16:09:19 +0100

    modificacion suma de totales en la salida

--- jvs/index.js
+++ jvs/index.js
@@ -69,10 +69,14 @@ while (cargarToken) {
 }
 
 let suma_total = 0
-contenido_tokens = ' '
+contenido_tokens = ''
 
-for (let tok of tokens_comprados) {
+/*for (let tok of tokens_comprados) {
   let token = new Producto(tok.id, tok.nombre, tok.precio)
-  suma_total += token.precio
+  suma_total += token.precio;
   alert('Total Token:\n\n' + contenido_tokens + '€' + suma_total)
-}
+} */
+tokens_comprados.forEach((tok)=> {
+  suma_total +=tok.precio;
+})
+alert('Total Token:\n\n' + contenido_tokens + '€' + suma_total);
