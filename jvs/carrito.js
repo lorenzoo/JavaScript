7b82045673acdf84c011266db70a223b6b059a7d
enrique lorenzo <enriquelorenzoo@gmail.com>
2023-01-11 20:59:11 +0100

    cambios en carrito.html css y js.carrito

--- jvs/carrito.js
+++ jvs/carrito.js
@@ -1,18 +1,17 @@
 
-
 /* AQUI EMPIEZA LA PARTE DEL JS DE CARRITO */
 
 let productosEnCarrito = localStorage.getItem("productos-en-carrito");
 productosEnCarrito = JSON.parse(productosEnCarrito);
 
-const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
-const contenedorCarritoProductos = document.querySelector("#carrito-productos");
-const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
-const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
-let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
-const botonVaciar = document.querySelector("#carrito-acciones-vaciar");
+const contenedorCarritoVacio = document.querySelector(".carrito-vacio");
+const contenedorCarritoProductos = document.querySelector(".carrito-productos");
+const contenedorCarritoAcciones = document.querySelector(".carrito-acciones");
+const contenedorCarritoComprado = document.querySelector(".carrito-comprado");
+let botonesEliminar = document.querySelectorAll("#carrito-producto-eliminar");
+const botonVaciar = document.querySelector(".carrito-acciones-vaciar");
 const contenedorTotal = document.querySelector("#total");
-const botonComprar = document.querySelector("#carrito-acciones-comprar");
+const botonComprar = document.querySelector(".carrito-acciones-comprar");
 
 
 function cargarProductosCarrito() {
@@ -46,9 +45,12 @@ function cargarProductosCarrito() {
                 <div class="carrito-producto-subtotal">
                     <small>Subtotal</small>
                     <p>$${producto.precio * producto.cantidad}</p>
-                </div>
-                <button class="carrito-producto-eliminar" id="${producto.id}"><i class="bi bi-trash-fill"></i></button>
-            `;
+                </div>                                                           
+                <button class="carrito-producto-eliminar" id="${producto.id}"><svg xmlns="http://www.w3.org/2000/svg" width="16"  
+            height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+            `;                                                                         
+        /*<button class="carrito-producto-eliminar" id="${producto.id}"><i class="bi bi-trash-fill"></i></button>*/
     
             contenedorCarritoProductos.append(div);
         })
